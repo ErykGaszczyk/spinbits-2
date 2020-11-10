2a5709c108d6604d22e1c29365dcd8f06645081a
darthmanu <manowski.b@gmail.com>
2020-11-10 14:44:50 +0100

    feature: added correct graphql request

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -19,5 +19,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: 'CMS',
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: 'cms',
+        // Url to query from
+        url: 'http://localhost:1337/graphql',
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA1MDA0MjA2LCJleHAiOjE2MDc1OTYyMDZ9.TRDgLW3occlOY7PMRTIC39JGZmDIc4ZXO17zIcYaklU`,
+        },
+      },
+    },
   ],
 };
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Test from '@components/Test';
+import { useStaticQuery, graphql } from 'gatsby';
 import GlobalStyle from '../styles';
 
-export default function Home() {
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      cms {
+        blogPosts {
+          id
+          title
+          test {
+            ... on CMS_ComponentBlogCustomMeta {
+              id
+              key
+              value
+            }
+            ... on CMS_ComponentBlogAuthor {
+              picture {
+                id
+              }
+              Name
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <div>
       <GlobalStyle />
       <Test title="Hello Mando!" />
+      {console.log(data)}
     </div>
   );
-}
+};
+
+export default Home;
