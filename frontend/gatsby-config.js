9a1021c836339e7e908227cde7ea592d8f572f21
darthmanu <manowski.b@gmail.com>
2020-11-10 17:54:25 +0100

    feature: gatsby request

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   plugins: [
@@ -27,10 +30,10 @@ module.exports = {
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: 'cms',
         // Url to query from
-        url: 'http://localhost:1337/graphql',
+        url: `${process.env.GRAPHQL_PLAYGROUND_URL}`,
         headers: {
           // Learn about environment variables: https://gatsby.dev/env-vars
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA1MDA0MjA2LCJleHAiOjE2MDc1OTYyMDZ9.TRDgLW3occlOY7PMRTIC39JGZmDIc4ZXO17zIcYaklU`,
+          Authorization: `Bearer ${process.env.AUTHORIZATION_KEY}`,
         },
       },
     },
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import Test from '@components/Test';
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 import GlobalStyle from '../styles';
+
+const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+const PostDiv = styled.div`
+  margin: 0 0 5rem 0;
+  background-color: #${randomColor};
+`;
+
+const Li = styled.li`
+  margin: 0 0 2rem 0;
+
+  table,
+  th,
+  td {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+`;
 
 const Home = () => {
   const data = useStaticQuery(graphql`
-    query MyQuery {
+    query getBlogPosts {
       cms {
         blogPosts {
           id
           title
+          created_at
           test {
             ... on CMS_ComponentBlogCustomMeta {
               id
@@ -19,6 +39,7 @@ const Home = () => {
             ... on CMS_ComponentBlogAuthor {
               picture {
                 id
+                url
               }
               Name
             }
@@ -28,12 +49,71 @@ const Home = () => {
     }
   `);
 
+  const { blogPosts } = data.cms;
+
+  const renderCustomMeta = () => {
+    return blogPosts.map((item) => {
+      return (
+        <PostDiv key={`${item.id}-${Math.random()}`}>
+          <p>ID: {item.id}</p>
+          <p>
+            Post Title: {item.title}, Created {item.created_at}
+          </p>
+          <p>Post content:</p>
+          <ul>
+            {item.test
+              .filter((el) => el.__typename === 'CMS_ComponentBlogCustomMeta')
+              .map((filteredItems) => {
+                return (
+                  <Li key={`${item.id}-${Math.random()}`}>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <th>id</th>
+                          <th>key</th>
+                          <th>value</th>
+                        </tr>
+                        <tr>
+                          <td>{filteredItems.id}</td>
+                          <td>{filteredItems.key}</td>
+                          <td>{filteredItems.value}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </Li>
+                );
+              })}
+          </ul>
+          <>
+            {item.test
+              .filter((el) => el.__typename === 'CMS_ComponentBlogAuthor')
+              .map((filteredItems) => {
+                return (
+                  <div key={`${filteredItems.Name}-${Math.random()}`}>
+                    <p>Author Name: {filteredItems.Name}</p>
+                    <img
+                      src={
+                        filteredItems.picture === null
+                          ? `https://via.placeholder.com/600x200/000000/FFFFFF/?text=Author: ${filteredItems.Name}`
+                          : `http://localhost:1337${filteredItems.picture.url}`
+                      }
+                      alt={filteredItems.Name}
+                    />
+                  </div>
+                );
+              })}
+          </>
+        </PostDiv>
+      );
+    });
+  };
+
   return (
-    <div>
+    <>
       <GlobalStyle />
       <Test title="Hello Mando!" />
-      {console.log(data)}
-    </div>
+      {renderCustomMeta()}
+    </>
   );
 };
 
