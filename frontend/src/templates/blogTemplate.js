17242ae1a485e850776be0d13b68de8ad3996f69
darthmanu <manowski.b@gmail.com>
2020-11-19 14:00:16 +0100

    fix: comment

--- frontend/src/templates/blogTemplate.js
+++ frontend/src/templates/blogTemplate.js
@@ -90,6 +90,9 @@ const Template = ({ data }) => {
 
     return blogContentWithoutFirstSimpleText.map((item) => {
       const renderFunction = functionMap[item.__typename];
+      if (typeof functionMap[item.__typename] === 'undefined') {
+        throw new Error(`${item.__typename} type is unsupported`);
+      }
       const returnValue = renderFunction(item);
 
       return returnValue;
