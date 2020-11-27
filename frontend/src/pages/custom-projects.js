4381fa10bacd3dfcac8096c1af4042183b915125
darthmanu <manowski.b@gmail.com>
2020-11-27 15:01:04 +0100

    fix: error

--- frontend/src/pages/custom-projects.js
+++ frontend/src/pages/custom-projects.js
@@ -3,14 +3,10 @@ import Layout from '@components/Layout';
 import OurExpertise from '@components/sections/OurExpertise';
 
 const customProjects = () => {
-  const renderSections = () => {
-    return [OurExpertise()];
-  };
-
   return (
     <Layout>
       <h1>Custom projects</h1>
-      {renderSections()}
+      <OurExpertise />
     </Layout>
   );
 };
