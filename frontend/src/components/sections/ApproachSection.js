28b4f51db01b0d62d3b760d408e0ca58f481ea85
darthmanu <manowski.b@gmail.com>
2020-11-27 17:39:26 +0100

    feature: approach component

--- frontend/src/components/sections/ApproachSection.js
+++ frontend/src/components/sections/ApproachSection.js
@@ -1,15 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { Row, Col } from '@bootstrap-styled/v4';
 import { SpinContainer } from '@components/overrides';
 import SectionTopTitle from '@components/typography/SectionTopTitle';
 import SectionTitle from '@components/typography/SectionTitle';
-import Paragraph from '@components/typography/Paragraph';
-// import { P } from '@components/typography/Paragraph.styled';
+import Paragraph, { BasicText } from '@components/typography/Paragraph';
+import SpinButton from '@components/SpinButton';
+
+const CustomColumn = styled(Col)`
+  margin: 2rem 0;
+  ${(props) => props.flexEnd && 'display: flex; justify-content: flex-end'}
+`;
+
+const ColumnBox = styled.div`
+  background: var(--white);
+  box-shadow: -15px 15px 54px var(--shadow);
+  border-radius: 0.5rem;
+  padding: 30px 23px;
+  min-height: 450px;
+  height: 100%;
+`;
+
+const ColumnTitle = styled.h3`
+  ${BasicText}
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: var(--primary-font-color);
+`;
+
+const Li = styled.li`
+  ${BasicText}
+`;
 
 const ApproachSection = ({ data }) => {
-  console.log(data);
+  const renderColumnList = (listData) => {
+    return (
+      <ul key={listData}>
+        {listData.map((item) => {
+          return <Li key={`${item}`}>{item}</Li>;
+        })}
+      </ul>
+    );
+  };
+
+  const renderColumns = () => {
+    return data.map((item, index) => {
+      const { id, title, description } = item;
+      return (
+        <CustomColumn
+          md={6}
+          lg={4}
+          xl={3}
+          key={`${id}-${title}`}
+          data-sal="slide-up"
+          data-sal-easing="easeOutCubic"
+          data-sal-delay={100 * index}
+        >
+          <ColumnBox>
+            <ColumnTitle>{title}</ColumnTitle>
+            {description.map((el) => {
+              return typeof el === 'string' ? (
+                <Paragraph key={el}>{el}</Paragraph>
+              ) : (
+                renderColumnList(el)
+              );
+            })}
+          </ColumnBox>
+        </CustomColumn>
+      );
+    });
+  };
+
   return (
     <SpinContainer>
       <Row>
@@ -24,6 +86,23 @@ const ApproachSection = ({ data }) => {
           </Paragraph>
         </Col>
       </Row>
+      <Row>{renderColumns()}</Row>
+      <Row>
+        <Col>
+          <Paragraph>
+            In spinbits, we understand how important it is to be agile and open - that’s why we
+            won’t impose any approach. If you would like to work on the project your way - we are
+            ready to talk about that! We’ve worked on dozens of various projects - we are sure that
+            we will find a common language.
+          </Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <CustomColumn flexEnd="true">
+          {/* TODO: url do modala */}
+          <SpinButton url="/">Free project estimation</SpinButton>
+        </CustomColumn>
+      </Row>
     </SpinContainer>
   );
 };
@@ -33,7 +112,7 @@ ApproachSection.propTypes = {
     PropTypes.shape({
       id: PropTypes.number,
       title: PropTypes.string,
-      description: PropTypes.oneOfType(PropTypes.string, PropTypes.arrayOf(PropTypes.string)),
+      description: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
     })
   ).isRequired,
 };
--- frontend/src/components/typography/Paragraph.js
+++ frontend/src/components/typography/Paragraph.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'styled-components';
 import { P } from './Paragraph.styled';
+
+export const BasicText = css`
+  font-size: 1rem;
+  margin: 0 0 1rem 0;
+  color: var(--thirdary-font-color);
+  font-weight: ${(props) => (!props.bold ? 'normal' : '800')};
+  transition: 0.5s;
+  ${(props) => props.center && `text-align: center`};
+
+  strong {
+    ${(props) => props.coloredStrong && `color: var(--light-font-color)`};
+  }
+
+  :hover {
+    color: ${(props) => props.hover && `var(--secondary-font-color)`};
+  }
+`;
 
 const Paragraph = ({ children, bold, hover, coloredStrong, center }) => (
   <P bold={bold} hover={hover} coloredStrong={coloredStrong} center={center}>
