bbd227f91d4d680d5147f5355cb1d5c018b8b8b6
darthmanu <manowski.b@gmail.com>
2020-11-30 13:13:02 +0100

    feature: added slider to devops page

--- frontend/src/pages/devops.js
+++ frontend/src/pages/devops.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@components/Layout';
+import TechnologiesSliderSection from '@components/sections/TechnologiesSliderSection';
 import ApproachSection from '@components/sections/ApproachSection';
 import { devopsData } from '@utils/pages/devops-data';
 
@@ -8,6 +9,7 @@ const devops = () => {
     <Layout>
       <h1>Devops</h1>
       <ApproachSection data={devopsData} />
+      <TechnologiesSliderSection />
     </Layout>
   );
 };
