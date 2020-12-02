508ec1ded39ae0715d543c21d4ecb71533088075
darthmanu <manowski.b@gmail.com>
2020-12-02 14:24:38 +0100

    feature: validate inputs

--- frontend/src/components/sections/ContactFormSection.js
+++ frontend/src/components/sections/ContactFormSection.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 // import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import SectionTopTitle from '@components/typography/SectionTopTitle';
 // import SectionTitle from '@components/typography/SectionTitle';
 import { BasicText } from '@components/typography/Paragraph';
@@ -8,7 +9,9 @@ import { SpinContainer } from '@components/overrides';
 import { Button } from '@components/SpinButton';
 import { faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Row, Col, InputGroup, Input, InputGroupAddon } from '@bootstrap-styled/v4';
+import { Row, Col, InputGroup, InputGroupAddon } from '@bootstrap-styled/v4';
+
+// const axios = require('axios');
 
 const FormContainer = styled.form`
   background-color: var(--primary-font-color);
@@ -25,23 +28,10 @@ const FormTitle = styled.h3`
 
 const CustomInputGroup = styled(InputGroup)`
   margin: 0 0 1rem 0;
-
+  display: flex;
+  flex-direction: column;
   .input-group-addon {
     background-color: var(--white);
-  }
-
-  .form-control {
-    padding: 0.5rem 0.75rem 0.5rem 0;
-    border-color: var(--white);
-
-    &--textarea {
-      padding: 0.5rem 0.75rem;
-      height: 6rem;
-    }
-
-    &:focus {
-      border-color: var(--white);
-    }
   }
 `;
 
@@ -55,27 +45,89 @@ const SendButton = styled.button`
   }
 `;
 
+const InputFlexBox = styled.div`
+  display: flex;
+`;
+
+const SpinInput = css`
+  border-radius: 0.25rem;
+  border: 1px solid var(--white);
+  padding: 0.5rem 0.75rem 0.5rem 0;
+  width: 100%;
+`;
+const Input = styled.input`
+  ${SpinInput}
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+`;
+
+const Textarea = styled.textarea`
+  ${SpinInput}
+  padding: 0.5rem 0.75rem;
+  height: 6rem;
+
+  &:focus {
+    border-color: var(--white);
+  }
+`;
+
+const ErrorMessage = styled.p`
+  ${BasicText}
+  margin: 1rem 0 0 0;
+  color: var(--secondary-font-color);
+`;
+
 const ContactFormSection = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const { register, handleSubmit, errors } = useForm();
+  if (errors) console.log(errors);
+
+  const errorMessages = {
+    name_surname: {
+      required: '*This field is required.',
+      minLength: '*Min length: 2',
+    },
+  };
+
+  const errorHandler = (field, type) => {
+    const fieldName = errorMessages[field];
+    const message = fieldName[type];
+    // console.log('fieldName', fieldName);
+    // console.log('message', message);
+
+    return <ErrorMessage bold>{message}</ErrorMessage>;
+  };
+
+  const onSubmit = (data) => console.log('data');
   // TODOFIX: id="contact" || id="contact-form"
 
-  console.log('Name: ', name.length);
-  console.log('Email: ', email);
-  console.log('Message: ', message);
+  // const reset = () => {
+  //   setName('');
+  //   setEmail('');
+  //   setMessage('');
+  // };
 
-  const reset = () => {
-    setName('');
-    setEmail('');
-    setMessage('');
-  };
+  // const onSumbit = () => {
+  //   // event.preventDefault();
+  //   // console.log('Name: ', name);
+  //   // console.log('Email: ', email);
+  //   // console.log('Message: ', message);
 
-  const handleSumbit = (event) => {
-    event.preventDefault();
-    alert('Dupa');
-    reset();
-  };
+  //   axios({
+  //     method: 'post',
+  //     url: `https://api.spinbits.io/emails/contact`,
+  //     data: {
+  //       name_surname: name,
+  //       email,
+  //       message,
+  //     },
+  //   })
+  //     .then((response) => {
+  //       console.log(response.status);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
   return (
     <SpinContainer id="contact">
@@ -86,7 +138,7 @@ const ContactFormSection = () => {
       </Row>
       <Row>
         <Col>
-          <FormContainer onSubmit={handleSumbit}>
+          <FormContainer onSubmit={handleSubmit(onSubmit)}>
             <Row>
               <Col xs={12}>
                 <FormTitle center bold>
@@ -95,40 +147,40 @@ const ContactFormSection = () => {
               </Col>
               <Col md={5}>
                 <CustomInputGroup>
-                  <InputGroupAddon>
-                    <FontAwesomeIcon icon={faUser} />
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    id="inlineFormInputGroup"
-                    placeholder="Name and surname"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
+                  <InputFlexBox>
+                    <InputGroupAddon>
+                      <FontAwesomeIcon icon={faUser} />
+                    </InputGroupAddon>
+                    <Input
+                      type="text"
+                      placeholder="Name and surname"
+                      name="name_surname"
+                      ref={register({ required: true, minLength: 3, maxLength: 20 })}
+                    />
+                  </InputFlexBox>
+                  {errors.name_surname !== undefined &&
+                    errorHandler(errors.name_surname.ref.name, errors.name_surname.type)}
                 </CustomInputGroup>
 
                 <CustomInputGroup>
-                  <InputGroupAddon>
-                    <FontAwesomeIcon icon={faEnvelope} />
-                  </InputGroupAddon>
-                  <Input
-                    type="text"
-                    id="inlineFormInputGroup"
-                    placeholder="E-mail"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+                  <InputFlexBox>
+                    <InputGroupAddon>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </InputGroupAddon>
+                    <Input
+                      type="text"
+                      placeholder="Email"
+                      name="email"
+                      ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+                    />
+                  </InputFlexBox>
+                  {errors.email && <ErrorMessage bold>*This is required.</ErrorMessage>}
                 </CustomInputGroup>
               </Col>
               <Col md={7}>
                 <CustomInputGroup>
-                  <textarea
-                    className="form-control form-control--textarea"
-                    name="contack_message"
-                    placeholder="Message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                  />
+                  <Textarea name="message" placeholder="Message" ref={register} />
+                  {errors.message && <ErrorMessage bold>*This is required.</ErrorMessage>}
                 </CustomInputGroup>
                 <SendButton type="submit">Send message</SendButton>
               </Col>
