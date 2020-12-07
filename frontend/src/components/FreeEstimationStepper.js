78b989cf0d9564cd0cfb9eaff3f1be833cc2a258
darthmanu <manowski.b@gmail.com>
2020-12-07 16:02:47 +0100

    feature: all steps

--- frontend/src/components/FreeEstimationStepper.js
+++ frontend/src/components/FreeEstimationStepper.js
@@ -3,14 +3,27 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Modal from 'react-modal';
 import Paragraph, { BasicText } from '@components/typography/Paragraph';
-import { Container, Row, Col } from '@bootstrap-styled/v4';
-import Check from '@images/stepper/check.webp';
+import { Button } from '@components/SpinButton';
 import CustomProject from '@images/stepper/custom_project.webp';
 import DigitalMarketing from '@images/stepper/digital_marketing.webp';
 import StaffOutsourcing from '@images/stepper/staff_outsourcing.webp';
 import SupportMaintenance from '@images/stepper/support_maintenance.webp';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container, Row, Col } from '@bootstrap-styled/v4';
+
+const CustomRow = styled(Row)`
+  margin: 0 0 2rem 0;
+`;
+
+const CustomColButton = styled(Col)`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const NextStepButton = styled.button`
+  ${Button}
+`;
 
 const Title = styled.p`
   ${BasicText}
@@ -71,21 +84,153 @@ const FontAwesome = styled(FontAwesomeIcon)`
 `;
 
 const FreeEstimationStepper = ({ openFromParent, parentCallback }) => {
-  const [modalIsOpen, setIsOpen] = useState(openFromParent);
-  const [serviceCheckbox, setServiceCheckbox] = useState(false);
-  console.log('serviceCheckbox: ', serviceCheckbox);
+  const serviceData = [
+    {
+      id: 1,
+      img: `${CustomProject}`,
+      title: `Custom Project`,
+      slug: `custom-project`,
+    },
+    {
+      id: 2,
+      img: `${SupportMaintenance}`,
+      title: `Support & Maintenance`,
+      slug: `support-maintenance`,
+    },
+    {
+      id: 3,
+      img: `${StaffOutsourcing}`,
+      title: `Staff Outsourcing`,
+      slug: `staff-outsourcing`,
+    },
+    {
+      id: 4,
+      img: `${DigitalMarketing}`,
+      title: `Digital Marketing`,
+      slug: `digital-marketing`,
+    },
+  ];
+  const [step, setStep] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(openFromParent);
 
   useEffect(() => {
     Modal.setAppElement('body');
-  }, [setIsOpen]);
+  }, [modalIsOpen, setModalIsOpen]);
 
   const closeModal = () => {
-    setIsOpen(false);
+    setModalIsOpen(false);
     parentCallback(false);
   };
 
+  console.log('step', step);
+
+  const nextStep = () => {
+    if (step < 4) setStep((origin) => origin + 1);
+  };
+
+  const renderServicesCheckbox = () => {
+    return serviceData.map((item) => {
+      const [serviceCheckbox, setServiceCheckbox] = useState(false);
+      const { id, img, title, slug } = item;
+      return (
+        <Col lg={3} key={`${id}-${slug}`}>
+          <ServiceBox serviceCheckbox={serviceCheckbox}>
+            <Checkbox
+              serviceCheckbox={serviceCheckbox}
+              onClick={() => setServiceCheckbox((origin) => !origin)}
+            >
+              {serviceCheckbox && <FontAwesome icon={faCheck} />}
+            </Checkbox>
+            <img src={img} alt="Spinbits - Custom Projects" />
+            <BoxTitle serviceCheckbox={serviceCheckbox} center primary bold>
+              {title}
+            </BoxTitle>
+          </ServiceBox>
+        </Col>
+      );
+    });
+  };
+
+  const renderServices = () => {
+    return (
+      <CustomRow>
+        <Col xs={12}>
+          <Paragraph bold>Service</Paragraph>
+        </Col>
+        {renderServicesCheckbox()}
+      </CustomRow>
+    );
+  };
+
+  const renderBudget = () => {
+    return (
+      <>
+        <CustomRow>
+          <Col xs={12}>
+            <Paragraph bold>Service</Paragraph>
+          </Col>
+          <Col xs={12}>
+            <Paragraph>rande slider</Paragraph>
+          </Col>
+        </CustomRow>
+      </>
+    );
+  };
+
+  const renderContactDetails = () => {
+    return (
+      <>
+        <CustomRow>
+          <Col xs={12}>
+            <Paragraph bold>Contact Details</Paragraph>
+          </Col>
+          <Col xs={12}>
+            <Paragraph>inputy</Paragraph>
+          </Col>
+        </CustomRow>
+      </>
+    );
+  };
+
+  const renderAdditionalInfo = () => {
+    return (
+      <>
+        <CustomRow>
+          <Col xs={12}>
+            <Paragraph bold>Additional info (optional)</Paragraph>
+          </Col>
+          <Col xs={12}>
+            <Paragraph>pliki</Paragraph>
+          </Col>
+        </CustomRow>
+      </>
+    );
+  };
+
+  const handleStepRender = () => {
+    return (
+      <>
+        {step >= 1 && renderServices()}
+        {step >= 2 && renderBudget()}
+        {step >= 3 && renderContactDetails()}
+        {step >= 4 && renderAdditionalInfo()}
+      </>
+    );
+  };
+
+  const renderButtons = () => {
+    return (
+      <Row>
+        <CustomColButton>
+          {step < 4 && <NextStepButton onClick={() => nextStep()}>Next Step</NextStepButton>}
+          {step === 4 && <NextStepButton onClick={() => nextStep()}>Send request</NextStepButton>}
+        </CustomColButton>
+      </Row>
+    );
+  };
+
   return (
-    <div>
+    <Container>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -98,45 +243,25 @@ const FreeEstimationStepper = ({ openFromParent, parentCallback }) => {
           content: {
             maxWidth: '65%',
             margin: '0 auto',
+            overflowX: 'hidden',
           },
         }}
       >
         <button type="button" onClick={closeModal}>
           close
         </button>
-        <Container>
-          <Row>
-            <Col>
-              <Title coloredStrong center bold>
-                Get a <strong>free</strong> estimation in <strong>24 hours</strong>
-              </Title>
-              <Subtitle center>Let us know about your idea or how can we help you.</Subtitle>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Paragraph bold>Service</Paragraph>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={3}>
-              <ServiceBox serviceCheckbox={serviceCheckbox}>
-                <Checkbox
-                  serviceCheckbox={serviceCheckbox}
-                  onClick={() => setServiceCheckbox((origin) => !origin)}
-                >
-                  {serviceCheckbox && <FontAwesome icon={faCheck} />}
-                </Checkbox>
-                <img src={CustomProject} alt="Spinbits - Custom Projects" />
-                <BoxTitle serviceCheckbox={serviceCheckbox} center primary bold>
-                  Website
-                </BoxTitle>
-              </ServiceBox>
-            </Col>
-          </Row>
-        </Container>
+        <CustomRow>
+          <Col>
+            <Title coloredStrong center bold>
+              Get a <strong>free</strong> estimation in <strong>24 hours</strong>
+            </Title>
+            <Subtitle center>Let us know about your idea or how can we help you.</Subtitle>
+          </Col>
+        </CustomRow>
+        {handleStepRender()}
+        {renderButtons()}
       </Modal>
-    </div>
+    </Container>
   );
 };
 
